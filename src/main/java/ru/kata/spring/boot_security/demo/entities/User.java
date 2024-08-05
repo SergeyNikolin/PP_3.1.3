aec72f1e9d68ee3dc68299cc54adf245a4f56296
Nikolin Sergey <nikolinsergei@mail.ru>
2024-08-05 12:27:40 +0300

    Изменен fetch type на LAZY

--- src/main/java/ru/kata/spring/boot_security/demo/entities/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/entities/User.java
@@ -24,7 +24,7 @@ public class User implements UserDetails {
     private String lastName;
     @Column (nullable = false)
     private int age;
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
